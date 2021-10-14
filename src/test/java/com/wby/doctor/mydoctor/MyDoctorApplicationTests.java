8eb0aefd5650c4bb972749c9c264fbbbc35dcca4
浪味仙 <angweixian@jamerly-inc.com>
2021-10-14 15:43:42 +0800

    Initial commit

---
+++ src/test/java/com/wby/doctor/mydoctor/MyDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wby.doctor.mydoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDoctorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
